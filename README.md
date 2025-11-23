f17d21269cc540ab93772cbeb7654daa6c9013ad
Charles Kolozsvary <charleskolozsvary@gmail.com>
2025-11-23 18:46:45 -0500

    extract.py (getCorrections() mostly) and README.md updated

--- README.md
+++ README.md
@@ -12,8 +12,34 @@ conda activate texpdfannots
 conda deactivate
 ```
 
+Delete an environment with
+```shell
+conda remove --name [env name] --all
+```
+
 ## python packages
 After activating the conda environment, run
 ```shell
 poetry install
 ```
+
+
+# TODO
+- Draw bounding boxes for text extraction
+- may need to find size of adjacent letters
+
+
+# Other Considerations
+
+## Annotations that select text on more than one line
+When an annotation selects text on more than one line, it's rectangle encompases all lines of selected text, no longer just the text itself (as it would be
+if the text selected belonged to just one line).
+
+To handle these cases I would need to
+1. Identify highlight, strikeout, or other multi-select annotations whose bounding boxes include more than one line.
+2. determine which line bounding boxes the annotation visual intersections with
+3. Create the selected text "surrounding text <selected>text where the visual annotation intersects the line</selected> more surrounding
+
+It's possible that 2. can be done by reading information from the PDF, but more likely I would need to process that region of the pdf as an image and
+use some computer vision methods.
+
--- src/texpdfannots/extract.py
+++ src/texpdfannots/extract.py
@@ -1,23 +1,29 @@
 import pymupdf
 import argparse
+import re
 from copy import deepcopy
 
 PDF_ANNOT_TEXT = (0, 'Text')
 PDF_ANNOT_STRIKE_OUT = (11, 'StrikeOut')
 PDF_ANNOT_CARET = (14, 'Caret')
 
+CARET_BUFF = 2 # points
+EXTRACT_TEXT_BUFFER_WIDTH = 2 # 2 pt
+
 class Annot:
-    """My own version of pymupdf's Annot which fixes the bounding box of the Caret annotation and isn't fragile. See getStableAnnots()"""
-    def __init__ (self, _pageno, _type, _info, _xref, _irt_xref, _rect):
+    """Revised version of pymupdf's Annot which fixes the bounding box of the Caret annotation and isn't fragile. See getStableAnnots()"""
+    def __init__ (self, _pageno, _type, _info, _xref, _irt_xref, _rect, _intersecting_line_bb):
         self.pageno = _pageno        
         self.type = _type
         self.info = _info                
         self.xref = _xref
         self.irt_xref = _irt_xref
         self.rect = _rect
+        self.intersecting_line_bb = _intersecting_line_bb
         
     def __str__ (self):
-        return str({'pageno':self.pageno,'type':self.type,'info':self.info,'xref':self.xref,'irt_xref':self.irt_xref,'rect':self.rect})
+        return str({'pageno':self.pageno,'type':self.type,'info':self.info,'xref':self.xref,
+                    'irt_xref':self.irt_xref,'rect':self.rect,'intersecting line bb':self.intersecting_line_bb})
     
     def __repr__ (self):
         return str(self)
@@ -35,17 +41,18 @@ class Edit:
                  which are identified by isReplaceAnnot in getCorrections(), not by pymupdf. 
     
     "message":   text in the annotation comment box and responses to it---typically edit directions
-                 if it's not already self-evident from the type (e.g., Strikeout)
+                 if it's not already self-evident from the type (e.g., Strikeout). The message itself
+                 will contain one string for the original comment and a list of strings for the responses
     
-    "selection": selected and surrounding text of the annotation.
+    "selection": selected and surrounding text of the annotation from the PDF.
 
     Example
     {
       "pageno" : 1, 
       "type" : "Replace" 
       "message": {
-                   "head": "Equation (1)", 
-                   "responses": ["COMP: pls link", "PE: okay?"]
+                   "comment": "Equation (1)", 
+                   "responses": ["COMP: pls link"]
                  }
       "selection": "Next we prove that <Replace>(1)</Replace> is a consequence of"
     }
@@ -69,32 +76,34 @@ def getStableAnnots(doc, draw_boxes = False):
     were inserted on, so they are resized to prevent that.
 
     pymupdf's annotations are also kind of fragile---they are strongly bound to the page they
-    come from, and I've encountered issues with using the provided methods to update the
-    annotations, so I'll just store the annotations with my own class which isn't tied to
-    the page and stores the information correctly.
+    come from (so when the page goes away, so does the annotation), and I've encountered issues
+    with using the provided methods to update the annotations, so I'll just store the
+    annotations with my own class which isn't tied to the page and correctly stores the information.
     """
     stable_annots = {pageno:[] for pageno in range(len(doc))}
     for pageno, page in enumerate(doc):
         blocks = page.get_text('dict', sort=True)['blocks']
-        line_bbs = []
         line_bbs = [line['bbox'] for block in blocks for line in block['lines']]
         for annot in page.annots():
-            if annot.type != (14, 'Caret'):
-                stable_annots[pageno].append(Annot(pageno, annot.type, annot.info, annot.xref, annot.irt_xref, annot.rect))
+            annotRect = annot.rect
+            intersecting_line_bbs = list(filter(lambda l: annotRect.intersects(l), line_bbs))
+
+            if annot.type == PDF_ANNOT_TEXT:
+                stable_annots[pageno].append(Annot(pageno,annot.type,annot.info,annot.xref,annot.irt_xref,annotRect,None))
                 continue
             
-            caretRect = annot.rect
-            intersecting_line_bbs = list(filter(lambda l: caretRect.intersects(l), line_bbs))
             # bbbox = (x0, y0, x1, y1)
             highest_baseline_bb = sorted(intersecting_line_bbs, key = lambda bb : bb[3])[0]
-            
-            raisedRect = pymupdf.Rect(caretRect.top_left, caretRect.bottom_right)
-            raisedRect.y1 = highest_baseline_bb[3]
 
-            stable_annots[pageno].append(Annot(pageno,annot.type,annot.info,annot.xref,annot.irt_xref,raisedRect))
+            if annot.type == PDF_ANNOT_CARET: #fix caret rect
+                raisedRect = pymupdf.Rect(annotRect.top_left, annotRect.bottom_right)
+                raisedRect.y1 = highest_baseline_bb[3]
+                annotRect = raisedRect
 
-            if draw_boxes:                           
-                orig = page.add_freetext_annot(caretRect, "", text_color=(0,1,0))
+            stable_annots[pageno].append(Annot(pageno,annot.type,annot.info,annot.xref,annot.irt_xref,annotRect,highest_baseline_bb))
+
+            if draw_boxes:               
+                orig = page.add_freetext_annot(annotRect, "", text_color=(0,1,0))
                 orig.set_border(width=.3)
                 orig.update()                                       
             
@@ -140,6 +149,32 @@ def getResponses(annot, all_responses):
     
     return resps_by_type
 
+def getSelection(ann, doc):
+    buff = EXTRACT_TEXT_BUFFER_WIDTH
+    selection_name = ann.type[1]
+    page = doc[ann.pageno]
+    x0, y0, x1, y1 = ann.intersecting_line_bb
+    
+    if selection_name == 'Caret':
+        insertion_point_x = ann.rect.x0 + ann.rect.width/2
+        left_rect = pymupdf.Rect(x0, y0, insertion_point_x-CARET_BUFF, y1)
+        right_rect = pymupdf.Rect(insertion_point_x+CARET_BUFF, y0, x1, y1)
+        return '{left}<Caret></Caret>{right}'.format(left = page.get_textbox(left_rect),
+                                                     right = page.get_textbox(right_rect))
+
+    elif re.match('(?:Replace|StrikeOut|Highlight|Underline)', selection_name):
+        left_rect = pymupdf.Rect(x0, y0, ann.rect.x0-buff, y1)
+        middle_rect = pymupdf.Rect(ann.rect.x0+buff/2, y0, ann.rect.x1-buff/2, y1)
+        # middle_rect = pymupdf.Rect(ann.rect.x0, y0, ann.rect.x1, y1)
+        right_rect = pymupdf.Rect(ann.rect.x1+buff, y0, x1, y1)
+        return '{left}<{name}>{middle}</{name}>{right}'.format(left = page.get_textbox(left_rect),
+                                                               middle = page.get_textbox(middle_rect),
+                                                               right = page.get_textbox(right_rect),
+                                                               name = selection_name)
+    else:
+        return None
+    
+
 def getCorrections(filename):
     """return a list of Edits. See class Edit."""
     doc = pymupdf.open(filename)
@@ -148,7 +183,6 @@ def getCorrections(filename):
     
     corrections = []
     for pageno, page in enumerate(doc):
-        blocks = page.get_text('dict', sort=True)['blocks']
         for annot in stable_annots[pageno]: 
             if annot.irt_xref != 0:
                 # only true for text responses and annotations which combine
@@ -156,26 +190,38 @@ def getCorrections(filename):
                 continue
             responses = getResponses(annot, all_responses)
             text_responses = responses[PDF_ANNOT_TEXT] if PDF_ANNOT_TEXT in responses else []
+            text_responses = [resp.info['content'] for resp in text_responses]
+            message = {'comment': annot.info['content'], 'responses': text_responses}            
 
             def isReplaceAnnot(ann, ann_resps):
                 if not (ann.type == PDF_ANNOT_STRIKE_OUT or ann.type == PDF_ANNOT_CARET) or ann_resps == []:
-                    return False
+                    return False, None
                 
                 assert ann.type not in ann_resps, "{} are in response to annotation of same type {}".format(str(ann_resps[ann.type]), str(ann))
                 assert len(ann_resps.keys()) <= 2, "ann {} has responses {} of more than two types".format(ann, ann_resps)
                 
                 other_ann_type = PDF_ANNOT_STRIKE_OUT if ann.type == PDF_ANNOT_CARET else PDF_ANNOT_CARET
                 if not (other_ann_type in ann_resps and len(ann_resps[other_ann_type]) == 1):
-                    return False
+                    return False, None
                 other_ann = ann_resps[other_ann_type][0]
 
-                # TODO: finish
-                return ann.rect.intersects(other_ann.rect) and other_ann.info['content'] == ''
+                return ann.rect.intersects(other_ann.rect) and other_ann.info['content'] == '', other_ann
                 
-            if isReplaceAnnot(annot, responses):
+            is_replace, other_ann = isReplaceAnnot(annot, responses)
+            
+            if is_replace:
+                if annot.type[1] == 'Caret':
+                    annot.rect = other_ann.rect
                 annot.type = (None, 'Replace')
+
+            selection_text = getSelection(annot, doc)
+
+            corrections.append(Edit(annot.pageno, annot.type[1], message, selection_text))
                 
-        return -1
+    return corrections
             
 if __name__ == '__main__':
-    print()
+    corrections = getCorrections('TeX/AnnotatedPDFS/ann0.pdf')
+    for cor in corrections:
+        print(cor)
+        print()
